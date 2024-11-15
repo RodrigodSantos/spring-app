c4bbf5c42c7ac63874c79d8bd0dde582ebdd2b89
Rodrigo dos Santos <94724162+RodrigodSantos@users.noreply.github.com>
2024-11-15 17:42:13 -0300

    fix bug

--- src/main/java/com/example/demo/gomes/spring_app/models/ProprietarioEnderecoModel.java
+++ src/main/java/com/example/demo/gomes/spring_app/models/ProprietarioEnderecoModel.java
@@ -2,6 +2,8 @@ package com.example.demo.gomes.spring_app.models;
 
 import java.util.UUID;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -23,6 +25,7 @@ public class ProprietarioEnderecoModel {
     private UUID id;
 
     @ManyToOne
+    @JsonBackReference
     @JoinColumn(name = "proprietario_id")
     private ProprietarioModel proprietario;
 
