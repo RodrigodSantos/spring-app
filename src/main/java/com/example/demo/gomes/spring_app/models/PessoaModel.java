c8a3560bdcf88e10f709080986c89dc274cceb77
Rodrigo dos Santos <94724162+RodrigodSantos@users.noreply.github.com>
2024-11-15 18:27:50 -0300

    create table tb_pessoa
    Model and Repository

---
+++ src/main/java/com/example/demo/gomes/spring_app/models/PessoaModel.java
@@ -0,0 +1,30 @@
+package com.example.demo.gomes.spring_app.models;
+
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "tb_pessoa")
+public class PessoaModel {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private String email;
+    private String dataNascimento;
+    private String sexo;
+    private String status;
+}
