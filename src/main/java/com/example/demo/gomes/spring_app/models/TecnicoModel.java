6d361a865b5aee39907640951976b3208fec60b6
Rodrigo dos Santos <94724162+RodrigodSantos@users.noreply.github.com>
2024-11-17 15:10:59 -0300

    fix

--- src/main/java/com/example/demo/gomes/spring_app/models/TecnicoModel.java
+++ src/main/java/com/example/demo/gomes/spring_app/models/TecnicoModel.java
@@ -1,12 +1,15 @@
 package com.example.demo.gomes.spring_app.models;
 
+import java.util.List;
 import java.util.UUID;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -25,5 +28,8 @@ public class TecnicoModel {
     @OneToOne
     @JoinColumn(name = "pessoa_id")
     private PessoaModel pessoa;
+
+    @OneToMany(mappedBy = "tecnico", cascade = CascadeType.ALL)
+    private List<TecnicoEnderecoModel> enderecos;
 
 }
