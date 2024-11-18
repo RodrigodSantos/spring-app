7e43223134868da00a1df77679f2841da50684ea
Rodrigo dos Santos <94724162+RodrigodSantos@users.noreply.github.com>
2024-11-18 20:53:49 -0300

    fix

--- src/main/java/com/example/demo/gomes/spring_app/controller/ProprietarioEnderecoController.java
+++ src/main/java/com/example/demo/gomes/spring_app/controller/ProprietarioEnderecoController.java
@@ -8,15 +8,12 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.gomes.spring_app.models.ProprietarioEnderecoModel;
-import com.example.demo.gomes.spring_app.service.EnderecoService;
 import com.example.demo.gomes.spring_app.service.ProprietarioEnderecoService;
-import com.example.demo.gomes.spring_app.service.ProprietarioService;
 
 @RestController
 @RequestMapping("/proprietario-endereco")
@@ -24,12 +21,6 @@ public class ProprietarioEnderecoController {
     
     @Autowired
     private ProprietarioEnderecoService proprietarioEnderecoService;
-
-    @Autowired
-    private ProprietarioService proprietarioService;
-
-    @Autowired
-    private EnderecoService enderecoService;
     
     @GetMapping()
     public List<ProprietarioEnderecoModel> getProprietarioEnderecos() {
@@ -43,16 +34,13 @@ public class ProprietarioEnderecoController {
 
     @PostMapping("")
     public ProprietarioEnderecoModel save(@RequestBody ProprietarioEnderecoModel proprietarioEndereco) {
-        proprietarioEndereco.setProprietario(proprietarioService.findById(proprietarioEndereco.getProprietario().getId()));
-        proprietarioEndereco.setEndereco(enderecoService.findById(proprietarioEndereco.getEndereco().getId()));
         return proprietarioEnderecoService.save(proprietarioEndereco);
     }
 
-    @PutMapping("/{id}")
-    public ProprietarioEnderecoModel update(@PathVariable UUID id, @RequestBody ProprietarioEnderecoModel proprietarioEndereco) {
-        proprietarioEnderecoService.findById(id);
-        return proprietarioEnderecoService.update(id, proprietarioEndereco);
-    }
+    // @PutMapping("/{id}")
+    // public ProprietarioEnderecoModel update(@PathVariable UUID id, @RequestBody EnderecoModel endereco) {
+    //     return proprietarioEnderecoService.update(id, endereco);
+    // }
 
     @DeleteMapping("/{id}")
     public void delete(@PathVariable UUID id) {
