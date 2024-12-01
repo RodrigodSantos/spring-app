ac046065d66db4778457acc11adc5df0531c7064
Rodrigo dos Santos <94724162+RodrigodSantos@users.noreply.github.com>
2024-12-01 19:52:39 -0300

    create table contato
    Model and Repository

---
+++ src/main/java/com/example/demo/gomes/spring_app/models/ContatoModel.java
@@ -0,0 +1,27 @@
+package com.example.demo.gomes.spring_app.models;
+
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "tb_contato")
+public class ContatoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    
+    private String tipo;
+    private String valor;
+    private Boolean isVisible = false;
+
+}
